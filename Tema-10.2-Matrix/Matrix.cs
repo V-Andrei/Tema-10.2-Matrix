d391fe8cd741bc2dd0aa2e04ccb87050da2555db
Andrei Ciprian Vrancianu <vrancianuandrei@gmail.com>
2019-12-20 15:09:36 +0200

    FirstUpload

---
+++ Tema-10.2-Matrix/Matrix.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Text;
+
+namespace Tema_10._2_Matrix
+{
+    // Define a class Matrix<T> to hold a matrix of numbers (e.g. integers, floats, decimals).
+    public class Matrix<T>
+    {
+        
+        private readonly T[,] matrix = null;
+
+        public Matrix(int rows, int columns, params T[] elements)
+        {
+            this.matrix = new T[rows, columns];
+            this.Rows = rows;
+            this.Columns = columns;           
+        }
+
+        public int Rows { get; set; }
+
+        public int Columns { get; set; }
+
+        // Problem 2 - Matrix indexer
+        public T this[int row, int col]
+        {
+            get
+            {
+                return this.matrix[row, col];
+            }
+            set
+            {
+                this.matrix[row, col] = value;
+            }
+        }
+
+        // Override ToString()
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int row = 0; row < this.Rows; row++)
+            {
+                for (int col = 0; col < this.Columns; col++)
+                    result.AppendFormat("{0,4}", this.matrix[row, col]);
+                result.AppendLine();
+            }
+
+            return result.ToString();
+        }
+
+        // (m1 + m2)
+        public static Matrix<T> operator +(Matrix<T> matrix1, Matrix<T> matrix2)
+        {
+            return AdditionSubtraction(matrix1, matrix2, true);
+        }
+
+        // (m1 - m2)
+        public static Matrix<T> operator -(Matrix<T> matrix1, Matrix<T> matrix2)
+        {
+            return AdditionSubtraction(matrix1, matrix2, false);
+        }
+
+        private static Matrix<T> AdditionSubtraction(Matrix<T> matrix1, Matrix<T> matrix2, bool op)
+        {
+            if (matrix1.Rows != matrix2.Rows || matrix1.Columns != matrix2.Columns)
+                throw new InvalidOperationException("Invalid operation!");
+
+            Matrix<T> result = new Matrix<T>(matrix1.Rows, matrix1.Columns);
+
+            for (int row = 0; row < result.Rows; row++)
+                for (int col = 0; col < result.Columns; col++)
+                    result[row, col] = matrix1[row, col] + (op ? matrix2[row, col] : -(dynamic)matrix2[row, col]);
+
+            return result;
+        }
+
+        // (m1 * m2)
+        public static Matrix<T> operator *(Matrix<T> matrix1, Matrix<T> matrix2)
+        {
+            Matrix<T> result = new Matrix<T>(matrix1.Rows, matrix2.Columns);
+
+            for (int row = 0; row < result.Rows; row++)
+                for (int col = 0; col < result.Columns; col++)
+                    for (int k = 0; k < matrix1.Columns; k++) 
+                        result[row, col] += (dynamic)matrix1[row, k] * matrix2[k, col];
+
+            return result;
+        }
+
+    }
+}
---
+++ Tema-10.2-Matrix/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Tema_10._2_Matrix
+{
+    class Program
+    {
+        static readonly Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            var matrix1 = new Matrix<int>(3, 3,
+            1, 2, 0,
+            0, 1, 1,
+            2, 0, 1);
+
+            var matrix2 = new Matrix<int>(3, 3);
+
+            for (int row = 0; row < matrix2.Rows; row++)
+                for (int col = 0; col < matrix2.Columns; col++)
+                    matrix2[row, col] = rnd.Next(10);
+
+            Console.WriteLine("First Matrix is:", matrix1.Rows, matrix1.Columns);
+            Console.WriteLine(matrix1);
+
+            Console.WriteLine("Second Matrix is:", matrix2.Rows, matrix2.Columns);
+            Console.WriteLine(matrix2);
+
+            Console.WriteLine("Add");
+            Console.WriteLine(matrix1 + matrix2);
+
+            Console.WriteLine("Sub");
+            Console.WriteLine(matrix1 - matrix2);
+
+            Console.WriteLine("Multiplication:");
+            Console.WriteLine(matrix1 * matrix2);
+
+            Console.ReadLine();
+        }
+    }
+}
